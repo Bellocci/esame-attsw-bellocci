d41c7f10fd72c3aec54d7bec0377c99737fcb42f
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-24 17:47:58 +0200

    unit test for newBook method of BookController class

--- esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/controller/BookController.java
+++ esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.examples.esame_attsw_Bellocci.controller;
 
+import com.examples.esame_attsw_Bellocci.model.Book;
 import com.examples.esame_attsw_Bellocci.model.Library;
 import com.examples.esame_attsw_Bellocci.repository.BookRepository;
 import com.examples.esame_attsw_Bellocci.view.BookView;
@@ -22,5 +23,19 @@ public class BookController {
 			return;
 		}
 		bookView.showAllBooks(bookRepository.getAllBooksOfLibrary(library.getId()));
+	}
+	
+	public void newBook(Library library, Book book) {
+		if(libraryController.getLibraryRepository().findLibraryById(library.getId()) == null) {
+			bookView.closeViewError("Doesnt exist library with id " + library.getId() + " ", library);
+			return;
+		}
+		Book book_found = bookRepository.findBookById(book.getId());
+		if(book_found != null) {
+			bookView.showError("Already existing book with id " + book_found.getId(), book_found);
+			return;
+		}
+		bookRepository.saveBookInTheLibrary(library, book);
+		bookView.bookAdded(book);
 	}
 }
--- esame_attsw_Bellocci/src/test/java/com/examples/esame_attsw_Bellocci/controller/BookControllerTest.java
+++ esame_attsw_Bellocci/src/test/java/com/examples/esame_attsw_Bellocci/controller/BookControllerTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -50,24 +51,84 @@ public class BookControllerTest {
 	
 	@Test
 	public void testAllBooksWhenLibraryIsContainedIntoDatabaseShouldReturnListOfBooksOfLibrary() {
+		// setup
 		Library library = new Library("1", "library1");
 		when(libraryController.getLibraryRepository()).thenReturn(libraryRepository);
 		when(libraryRepository.findLibraryById(library.getId())).thenReturn(library);
 		List<Book> books = asList(new Book());
 		when(bookRepository.getAllBooksOfLibrary(library.getId())).thenReturn(books);
+		
+		// exercise
 		bookController.allBooks(library);
+		
+		// verify
 		verify(bookView).showAllBooks(books);
 	}
 	
 	@Test
 	public void testAllBooksWhenLibraryDoesntExistIntoDatabaseShouldShowErrorInLibraryView() {
+		// setup
 		Library library = new Library("1", "library1");
 		when(libraryController.getLibraryRepository()).thenReturn(libraryRepository);
 		when(libraryRepository.findLibraryById(library.getId())).thenReturn(null);
 		
+		// exercise
 		bookController.allBooks(library);
+		
+		// verify
 		verify(bookView).closeViewError("Doesnt exist library with id 1 ", library);
 		verifyNoMoreInteractions(ignoreStubs(libraryRepository, libraryController));
 	}
-
+	
+	@Test
+	public void testNewBookWhenBookDoesntAlreadyExist() {
+		// setup
+		Library library = new Library("1", "library1");
+		Book book = new Book("1", "book1");
+		when(libraryController.getLibraryRepository()).thenReturn(libraryRepository);
+		when(libraryRepository.findLibraryById(library.getId())).thenReturn(library);
+		when(bookRepository.findBookById(book.getId())).thenReturn(null);
+		
+		// exercise
+		bookController.newBook(library, book);
+		
+		// verify
+		InOrder inOrder = inOrder(bookRepository, bookView);
+		inOrder.verify(bookRepository).saveBookInTheLibrary(library, book);
+		inOrder.verify(bookView).bookAdded(book);
+	}
+	
+	@Test
+	public void testNewBookWhenBookAlreadyExist() {
+		// setup
+		Library library = new Library("1", "library1");
+		Book already_added = new Book("1", "test");
+		Book new_book = new Book("1", "book1");
+		when(libraryController.getLibraryRepository()).thenReturn(libraryRepository);
+		when(libraryRepository.findLibraryById(library.getId())).thenReturn(library);
+		when(bookRepository.findBookById(new_book.getId())).thenReturn(already_added);
+		
+		// exercise
+		bookController.newBook(library, new_book);
+		
+		// verify
+		verify(bookView).showError("Already existing book with id 1", already_added);
+		verifyNoMoreInteractions(ignoreStubs(bookRepository, libraryRepository, libraryController));
+	}
+	
+	@Test
+	public void testNewBookWhenLibraryDoesntExistIntoDatabase() {
+		// setup
+		Library library = new Library("1", "library1");
+		Book new_book = new Book("1", "book1");
+		when(libraryController.getLibraryRepository()).thenReturn(libraryRepository);
+		when(libraryRepository.findLibraryById(library.getId())).thenReturn(null);
+		
+		// exercise
+		bookController.newBook(library, new_book);
+		
+		// verify
+		verify(bookView).closeViewError("Doesnt exist library with id 1 ", library);
+		verifyNoMoreInteractions(ignoreStubs(libraryRepository, libraryController));
+	}
 }
