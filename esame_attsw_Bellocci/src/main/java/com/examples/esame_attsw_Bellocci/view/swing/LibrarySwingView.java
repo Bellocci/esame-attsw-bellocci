02ecfa8c81b4cddc8236b8b9be41a7f404cb3e24
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-24 23:31:25 +0200

    Create testControlsInitialStates for LibrarySwingViewTest class

---
+++ esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/view/swing/LibrarySwingView.java
@@ -0,0 +1,204 @@
+package com.examples.esame_attsw_Bellocci.view.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import com.examples.esame_attsw_Bellocci.model.Library;
+import com.examples.esame_attsw_Bellocci.view.LibraryView;
+
+public class LibrarySwingView extends JFrame implements LibraryView {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtId;
+	private JTextField txtName;
+	private JButton btnAdd;
+	private JButton btnDelete;
+	private JScrollPane scrollPane;
+	private JLabel lblErrorMessage;
+	private JButton btnOpen;
+	private JList<Library> listLibraries;
+	private DefaultListModel<Library> listLibraryModel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LibrarySwingView frame = new LibrarySwingView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LibrarySwingView() {
+		setTitle("Library View");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 661, 383);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{57, 35, 179, 77, 360, 0};
+		gbl_contentPane.rowHeights = new int[]{19, 19, 25, 158, 25, 15, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		txtId = new JTextField();
+		txtId.setName("idTextBox");
+		
+		JLabel lblId = new JLabel("id");
+		lblId.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.gridwidth = 2;
+		gbc_lblId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+		
+		txtId.setColumns(10);
+		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.anchor = GridBagConstraints.NORTH;
+		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtId.insets = new Insets(0, 0, 5, 0);
+		gbc_txtId.gridwidth = 3;
+		gbc_txtId.gridx = 2;
+		gbc_txtId.gridy = 0;
+		contentPane.add(txtId, gbc_txtId);
+		
+		JLabel lblName = new JLabel("name");
+		lblName.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.gridwidth = 2;
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 1;
+		contentPane.add(lblName, gbc_lblName);
+		
+		txtName = new JTextField();
+		txtName.setName("nameTextBox");
+		txtName.setColumns(10);
+		GridBagConstraints gbc_txtName = new GridBagConstraints();
+		gbc_txtName.anchor = GridBagConstraints.NORTH;
+		gbc_txtName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtName.insets = new Insets(0, 0, 5, 0);
+		gbc_txtName.gridwidth = 3;
+		gbc_txtName.gridx = 2;
+		gbc_txtName.gridy = 1;
+		contentPane.add(txtName, gbc_txtName);
+		
+		btnAdd = new JButton("Add Library");
+		btnAdd.setEnabled(false);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.anchor = GridBagConstraints.NORTH;
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAdd.gridx = 3;
+		gbc_btnAdd.gridy = 2;
+		contentPane.add(btnAdd, gbc_btnAdd);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 4;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 3;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		listLibraryModel = new DefaultListModel<>();
+		listLibraries = new JList<>(listLibraryModel);
+		listLibraries.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listLibraries.setName("libraryList");
+		scrollPane.setViewportView(listLibraries);
+		
+		btnOpen = new JButton("Open Library");
+		btnOpen.setEnabled(false);
+		GridBagConstraints gbc_btnOpen = new GridBagConstraints();
+		gbc_btnOpen.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnOpen.insets = new Insets(0, 0, 5, 5);
+		gbc_btnOpen.gridx = 2;
+		gbc_btnOpen.gridy = 4;
+		contentPane.add(btnOpen, gbc_btnOpen);
+		
+		btnDelete = new JButton("Delete Library");
+		btnDelete.setEnabled(false);
+		GridBagConstraints gbc_btnDelete = new GridBagConstraints();
+		gbc_btnDelete.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnDelete.insets = new Insets(0, 0, 5, 0);
+		gbc_btnDelete.gridx = 4;
+		gbc_btnDelete.gridy = 4;
+		contentPane.add(btnDelete, gbc_btnDelete);
+		
+		lblErrorMessage = new JLabel(" ");
+		lblErrorMessage.setName("errorLabelMessage");
+		lblErrorMessage.setForeground(Color.RED);
+		lblErrorMessage.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblErrorMessage = new GridBagConstraints();
+		gbc_lblErrorMessage.anchor = GridBagConstraints.NORTH;
+		gbc_lblErrorMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblErrorMessage.gridwidth = 5;
+		gbc_lblErrorMessage.gridx = 0;
+		gbc_lblErrorMessage.gridy = 5;
+		contentPane.add(lblErrorMessage, gbc_lblErrorMessage);
+	}
+
+	@Override
+	public void showAllLibraries(List<Library> libraries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void libraryAdded(Library library) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void libraryRemoved(Library library) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showError(String error_message, Library library) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showAllBooksOfLibrary(Library library) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ esame_attsw_Bellocci/src/test/java/com/examples/esame_attsw_Bellocci/view/swing/LibrarySwingViewTest.java
@@ -0,0 +1,45 @@
+package com.examples.esame_attsw_Bellocci.view.swing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.core.matcher.JLabelMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+
+@RunWith(GUITestRunner.class)
+public class LibrarySwingViewTest extends AssertJSwingJUnitTestCase {
+	
+	private FrameFixture window;
+	
+	private LibrarySwingView librarySwingView;
+
+	@Override
+	protected void onSetUp() throws Exception {
+		GuiActionRunner.execute(() -> {
+			librarySwingView = new LibrarySwingView();
+			return librarySwingView;
+		});
+		window = new FrameFixture(robot(), librarySwingView);
+		window.show(); // shows the frame to test
+	}
+	
+	@Test @GUITest
+	public void testControlsInitialStates() {
+		window.label(JLabelMatcher.withText("id"));
+		window.textBox("idTextBox").requireEnabled();
+		window.label(JLabelMatcher.withText("name"));
+		window.textBox("nameTextBox").requireEnabled();
+		window.button(JButtonMatcher.withText("Add Library")).requireDisabled();
+		window.list("libraryList");
+		window.button(JButtonMatcher.withText("Delete Library")).requireDisabled();
+		window.button(JButtonMatcher.withText("Open Library")).requireDisabled();
+		window.label(JLabelMatcher.withText(" "));
+	}
+
+}
