5194a4a5fe6a59744fd0b3293f6d3aa4ea984bdf
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-25 08:20:34 +0200

    Used static access for EXIT_ON_CLOSE in LibrarySwingView class

--- esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/view/swing/LibrarySwingView.java
+++ esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/view/swing/LibrarySwingView.java
@@ -69,7 +69,7 @@ public class LibrarySwingView extends JFrame implements LibraryView {
 	 */
 	public LibrarySwingView() {
 		setTitle("Library View");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 661, 383);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
