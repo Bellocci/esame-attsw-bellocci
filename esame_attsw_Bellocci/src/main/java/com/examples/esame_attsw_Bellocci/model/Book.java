92fdcc7d8505fc6b8c9639675621f6904da630a1
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-25 12:49:39 +0200

    add again hashCode into Book class

--- esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/model/Book.java
+++ esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/model/Book.java
@@ -58,6 +58,16 @@ public class Book {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((library == null) ? 0 : library.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -83,4 +93,6 @@ public class Book {
 			return false;
 		return true;
 	}
+
+
 }
