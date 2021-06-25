750b37998f7b32337f129352bd3f03e5ede3aaa1
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-25 14:34:50 +0200

    modify equals method in Book class

--- esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/model/Book.java
+++ esame_attsw_Bellocci/src/main/java/com/examples/esame_attsw_Bellocci/model/Book.java
@@ -65,12 +65,6 @@ public class Book {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
 		Book book = (Book) obj;
 		if (id == null) {
 			if (book.id != null)
