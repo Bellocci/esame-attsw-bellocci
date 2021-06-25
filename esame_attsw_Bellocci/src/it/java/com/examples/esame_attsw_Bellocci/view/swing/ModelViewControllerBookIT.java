ae5962c482999f9e825bb3a4d9e288f0c133d0f7
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-06-25 03:55:18 +0200

    IT for MVC Book

---
+++ esame_attsw_Bellocci/src/it/java/com/examples/esame_attsw_Bellocci/view/swing/ModelViewControllerBookIT.java
@@ -0,0 +1,154 @@
+package com.examples.esame_attsw_Bellocci.view.swing;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Environment;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.MySQLContainer;
+
+import com.examples.esame_attsw_Bellocci.controller.BookController;
+import com.examples.esame_attsw_Bellocci.controller.LibraryController;
+import com.examples.esame_attsw_Bellocci.hibernate.util.HibernateUtil;
+import com.examples.esame_attsw_Bellocci.model.Book;
+import com.examples.esame_attsw_Bellocci.model.Library;
+import com.examples.esame_attsw_Bellocci.repository.BookRepository;
+import com.examples.esame_attsw_Bellocci.repository.LibraryRepository;
+import com.examples.esame_attsw_Bellocci.repository.mysql.BookMySQLRepository;
+import com.examples.esame_attsw_Bellocci.repository.mysql.LibraryMySQLRepository;
+
+@RunWith(GUITestRunner.class)
+public class ModelViewControllerBookIT extends AssertJSwingJUnitTestCase {
+
+	private FrameFixture window;
+	
+	private LibrarySwingView librarySwingView;
+	
+	private LibraryController libraryController;
+	private BookController bookController;
+	
+	private static LibraryRepository libraryRepository;
+	private BookRepository bookRepository;
+	
+	@SuppressWarnings("rawtypes")
+	private static MySQLContainer mySQLContainer;
+	
+	private static Properties settings;
+	
+	private Library library = new Library("1", "library1");;
+	
+	@SuppressWarnings({ "rawtypes", "resource" })
+	@BeforeClass
+	public static void setupServerAndHibernate() {
+		mySQLContainer = new MySQLContainer("mysql:8")
+				.withDatabaseName("test")
+				.withUsername("user")
+				.withPassword("password");
+		mySQLContainer.start();
+		
+		//Map<String, String> settings = new HashMap<>();
+		settings = new Properties();
+		
+		settings.put(Environment.DRIVER, mySQLContainer.getDriverClassName());
+		settings.put(Environment.URL, mySQLContainer.getJdbcUrl());
+		settings.put(Environment.USER, mySQLContainer.getUsername());
+		settings.put(Environment.PASS, mySQLContainer.getPassword());
+		settings.put(Environment.POOL_SIZE, "1");
+		settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+		settings.put(Environment.SHOW_SQL, "true");
+		settings.put(Environment.FORMAT_SQL, "true");
+		settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+		settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+		settings.put(Environment.HBM2DDL_HALT_ON_ERROR, "true");
+		settings.put(Environment.HBM2DDL_CREATE_SCHEMAS, "true");
+		settings.put(Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true");
+		
+		libraryRepository = new LibraryMySQLRepository(settings);
+	}
+	
+	@AfterClass
+	public static void shutdownServerAndHibernate() {
+		HibernateUtil.resetSessionFactory();
+		mySQLContainer.stop();
+	}
+
+	@Override
+	protected void onSetUp() throws Exception {
+		bookRepository = new BookMySQLRepository(settings);
+		libraryRepository.saveLibrary(library);
+		window = new FrameFixture(robot(), GuiActionRunner.execute(() -> {
+			librarySwingView = new LibrarySwingView();
+			libraryController = new LibraryController(librarySwingView, libraryRepository);
+			
+			BookSwingView bookSwingView = new BookSwingView();
+			bookSwingView.setLibrarySwingView(librarySwingView);
+			bookSwingView.setLibrary(library);
+			bookController = new BookController(bookRepository, bookSwingView, libraryController);
+			bookSwingView.setBookController(bookController);
+			return bookSwingView;
+		}));
+		window.show();
+	}
+	
+	@Override
+	protected void onTearDown() {
+		cleanDatabaseTables();
+	}
+	
+	private void cleanDatabaseTables() {
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+	        transaction = session.beginTransaction();
+	        List<Library> libraries = session.createQuery("FROM Library", Library.class).list();
+	        for(Library library: libraries)
+	        	session.delete(library);
+	        List<Book> books = session.createQuery("FROM Book", Book.class).list();
+	        for(Book book: books)
+	        	session.delete(book);
+	        transaction.commit();
+		} catch(Exception e) {
+			e.printStackTrace();
+			if(transaction != null)
+				transaction.rollback();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+	}
+	
+	@Test
+	public void testAddBook() {
+		window.textBox("idTextBox").enterText("1");
+		window.textBox("nameTextBox").enterText("book1");
+		window.button(JButtonMatcher.withText("Add book")).click();
+		Book book = new Book("1", "book1");
+		book.setLibrary(library);
+		assertThat(bookRepository.findBookById("1")).isEqualTo(book);
+	}
+	
+	@Test
+	public void testDeleteBook() {
+		Book book = new Book("1", "book1");
+		book.setLibrary(library);
+		bookRepository.saveBookInTheLibrary(library, book);
+		GuiActionRunner.execute(() -> bookController.allBooks(library));
+		window.list("bookList").selectItem(0);
+		window.button(JButtonMatcher.withText("Delete book")).click();
+		assertThat(bookRepository.findBookById(book.getId())).isNull();
+	}
+
+}
